a4575a70db91fc60dc82014d69b8274ba232ac88
cookie li (李凯泉) <likaiquan@inspur.com>
2023-09-14 11:10:17 +0800

    feat: 优化husky初始化方式

--- core/actions/init.js
+++ core/actions/init.js
@@ -1,56 +1,56 @@
-const fs = require('fs-extra')
-const { exec: spawn, execAsync, spinnerStart } = require('../../tools/utils')
-const log = require('../../tools/log')
+const fs = require("fs-extra");
+const { exec: spawn, execAsync, spinnerStart } = require("../../tools/utils");
+const log = require("../../tools/log");
 
-const EXTENSIONS_TEMPLATE =
-{
-    "recommendations": [
-        "dbaeumer.vscode-eslint",
-    ]
-}
+const EXTENSIONS_TEMPLATE = {
+  recommendations: ["dbaeumer.vscode-eslint"],
+};
 const SETTINGS_TEMPLATE = {
-    "editor.codeActionsOnSave": {
-        "source.fixAll.eslint": true
-    }
-}
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true,
+  },
+};
 
 const IGNORE_TEMPLATE = `
 # coo-moat
 !.vscode/settings.json
 !.vscode/extensions.json
-    `
+    `;
 const DEPENDENCIES = [
-    'eslint@~8.37.0',
-    '@commitlint/config-conventional@~17.4.4',
-    '@commitlint/cli@~17.5.1',
-    'husky@~8.0.3',
-    'lint-staged@~13.2.0'
-]
+  "eslint@~8.37.0",
+  "@commitlint/config-conventional@~17.4.4",
+  "@commitlint/cli@~17.5.1",
+  "husky@~8.0.3",
+  "lint-staged@~13.2.0",
+];
 
 function init() {
-    try {
-        const spinner =spinnerStart('正在安装依赖')
-        // 安装依赖
-        installPkg(DEPENDENCIES, () => {
-            spinner.stop()
-            log.success('依赖安装成功！')
-            // 初始化eslint配置文件
-            initEslintConfig()
-            // 初始化commitlint配置文件
-            initCommitintConfig()
-            // 初始化husky配置文件
-            initHuskyConfig()
-            // 初始化pkg.json中的husky配置
-            initLintstagedConfig()
-            // 更新[.vscode]中的配置文件
-            updateVscodeConfig()
-            // 更新[.gitionore]
-            updateIgnoreFile()
-        }, ['-D'])
-
-    } catch (e) {
-        log.error(e)
-    }
+  try {
+    const spinner = spinnerStart("正在安装依赖");
+    // 安装依赖
+    installPkg(
+      DEPENDENCIES,
+      () => {
+        spinner.stop();
+        log.success("依赖安装成功！");
+        // 初始化eslint配置文件
+        initEslintConfig();
+        // 初始化commitlint配置文件
+        initCommitintConfig();
+        // 初始化pkg.json中的husky初始化命令
+        initHuskyConfig();
+        // 初始化pkg.json中的lint-stage配置
+        initLintstagedConfig();
+        // 更新[.vscode]中的配置文件
+        updateVscodeConfig();
+        // 更新[.gitionore]
+        updateIgnoreFile();
+      },
+      ["-D"]
+    );
+  } catch (e) {
+    log.error(e);
+  }
 }
 
 /**
@@ -60,74 +60,80 @@ function init() {
  * @param {*} args 命令参数
  */
 function installPkg(pkgNames, callBack, args = []) {
-    const PREFIX = process.env.CLI_PACKAGE_MANAGER ?? 'npm'
-    const installer = spawn(PREFIX, ['install', ...pkgNames, ...args])
-    installer.stdout.on('data', function (data) {
-        log.info(data)
-    })
-    installer.stderr.on('data', function (data) {
-        log.info(data)
-    })
-    installer.on('close', function (code) {
-        if (code !== 0) {
-            throw (code)
-        } else {
-            callBack?.()
-        }
-    })
+  const PREFIX = process.env.CLI_PACKAGE_MANAGER ?? "npm";
+  const installer = spawn(PREFIX, ["install", ...pkgNames, ...args]);
+  installer.stdout.on("data", function (data) {
+    log.info(data);
+  });
+  installer.stderr.on("data", function (data) {
+    log.info(data);
+  });
+  installer.on("close", function (code) {
+    if (code !== 0) {
+      throw code;
+    } else {
+      callBack?.();
+    }
+  });
 }
 
 /**
  * 初始化pkg.json中的husky配置
  */
 function initLintstagedConfig() {
-    rewriteJson('package.json', {
-        "lint-staged": {
-            "*.{js,vue}": [
-                "eslint --fix"
-            ]
-        }
-    })
+  rewriteJson(
+    (fileJson) => {
+      fileJson["lint-staged"] = {
+        "*.{js,ts,jsx,tsx,vue}": ["eslint --fix"],
+      };
+      return fileJson;
+    },
+    { path: "package.json" }
+  );
 }
 
 /**
- * 初始化husky配置文件
+ * 初始化pkg.json中的husky初始化命令
  */
-async function initHuskyConfig() {
-    // 生成.husky目录命令
-    const dirCmd = ['husky', 'install'];
-    // 添加pre-commit钩子命令
-    const preCommitCmd = ['husky', 'add', '.husky/pre-commit', 'npx lint-staged']
-    //添加commit-msg钩子命令
-    const commitMsgCmd = ['husky', 'add', '.husky/commit-msg', 'npx --no-install commitlint --edit $1'];
-    await execAsync("npx", dirCmd, {
-        stdio: "inherit"
-    });
-    await execAsync("npx", preCommitCmd, {
-        stdio: "inherit"
-    });
-    await execAsync("npx", commitMsgCmd, {
-        stdio: "inherit"
-    });
+function initHuskyConfig() {
+  rewriteJson(
+    (fileJson) => {
+      const { scripts } = fileJson;
+      const cmdKey = "moat:init-husky";
+      const inithuskyCmd =
+        'rm -rf .husky && npx husky install && npx husky add .husky/commit-msg "npx --no-install commitlint --edit $1" && npx husky add .husky/pre-commit "npx lint-staged" ';
+      scripts[cmdKey] = inithuskyCmd;
+      if (scripts.prepare) {
+        scripts.prepare = scripts.prepare + ` && npm run ${cmdKey}`;
+      } else {
+        scripts.prepare = `npm run ${cmdKey}`;
+      }
+      return fileJson;
+    },
+    { path: "package.json" }
+  );
 }
 
 /**
  * 初始化commitlint配置文件
  */
 function initCommitintConfig() {
-    spawn("echo module.exports = { extends: ['@commitlint/config-conventional'] }",
-        ['>', 'commitlint.config.js'], {
-        stdio: 'inherit'
-    })
+  spawn(
+    "echo module.exports = { extends: ['@commitlint/config-conventional'] }",
+    [">", "commitlint.config.js"],
+    {
+      stdio: "inherit",
+    }
+  );
 }
 
 /**
  * 初始化eslint配置文件
  */
 function initEslintConfig() {
-    spawn('npx', ['eslint', '--init'], {
-        stdio: 'inherit'
-    })
+  spawn("npx", ["eslint", "--init"], {
+    stdio: "inherit",
+  });
 }
 
 /**
@@ -135,10 +141,10 @@ function initEslintConfig() {
  *  --使[.vscode]中的extensions.json、settings.json可提交至仓库
  */
 function updateIgnoreFile() {
-    fs.outputFile('.gitignore', IGNORE_TEMPLATE, { flag: 'a' }, err => {
-        if (err) throw err
-        log.success(`.gitignore已修改`)
-    })
+  fs.outputFile(".gitignore", IGNORE_TEMPLATE, { flag: "a" }, (err) => {
+    if (err) throw err;
+    log.success(`.gitignore已修改`);
+  });
 }
 
 /**
@@ -147,12 +153,22 @@ function updateIgnoreFile() {
  *  --settings.json
  */
 function updateVscodeConfig() {
-    const extensionsFilePath = '.vscode/extensions.json'
-    const settingsFilePath = '.vscode/settings.json'
-    // 修改[.vscode] extensions.json
-    rewriteJson(extensionsFilePath, EXTENSIONS_TEMPLATE)
-    // 修改[.vscode] settings.json
-    rewriteJson(settingsFilePath, SETTINGS_TEMPLATE)
+  const extensionsFilePath = ".vscode/extensions.json";
+  const settingsFilePath = ".vscode/settings.json";
+  // 修改[.vscode] extensions.json
+  rewriteJson(
+    (fileJson) => {
+      return { ...fileJson, ...EXTENSIONS_TEMPLATE };
+    },
+    { path: extensionsFilePath }
+  );
+  // 修改[.vscode] settings.json
+  rewriteJson(
+    (fileJson) => {
+      return { ...fileJson, ...SETTINGS_TEMPLATE };
+    },
+    { path: settingsFilePath }
+  );
 }
 
 /**
@@ -161,25 +177,17 @@ function updateVscodeConfig() {
  * @param {*} template 写入的内容
  * @param {*} styles 文件内容格式美化
  */
-function rewriteJson(path, template, styles = { spaces: '\t' }) {
+function rewriteJson(callBack, options) {
+  try {
+    const { path, styles = { spaces: "\t" } } = options;
     // 确保目标JSON文件存在，若不存在则创建
-    fs.ensureFile(path).then(() => {
-        // 获取原有的目标JSON文件内容
-        let fileJson = fs.readJsonSync(path, { throws: false })
-        // 若内容不为空则向原有内容中追加
-        if (fileJson) {
-            fileJson = { ...fileJson, ...template }
-        } else {
-            // 若内容为空则直接写入新内容
-            fileJson = template
-        }
-        fs.writeJson(path, fileJson, styles, err => {
-            if (err) throw err
-            log.success(`${path}已修改`)
-        })
-    }).catch(err => {
-        throw (err)
-    })
+    fs.ensureFileSync(path);
+    const fileJson = callBack(fs.readJsonSync(path, { throws: false }));
+    fs.writeJSONSync(path, fileJson, styles);
+    log.success(`${path}已修改`);
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
-module.exports = init
+module.exports = init;
