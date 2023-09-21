adee2040d98ea641c445d3bfa9859fc7398735ea
HOME\likaiquan <ewwf>
2023-09-21 17:16:36 +0800

    chore: 优化工程打包

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,5 +4,6 @@ esbuild.build({
     entryPoints: ['core/bin/index.js'],
     bundle: true,
     platform:'node',
+    minify: true,
     outfile: 'dist/core/coo-moat.js',
   }).catch(() => process.exit(1))
